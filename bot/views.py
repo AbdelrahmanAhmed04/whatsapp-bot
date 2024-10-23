04bd51f20d09d3f3ab90238306a6c463930d0655
AbdelrahmanAhmed04 <abdelrahman.ahmed.mail@gmail.com>
2024-10-23 22:20:22 +0300

    correcting the creds again

--- bot/views.py
+++ bot/views.py
@@ -6,7 +6,7 @@ import json
 # Create your views here.
 
 account_sid = 'AC9076f2e034e87ed36e71137fe8bc4ce9'
-auth_token = '571b81b4b3b4d6cf8bbcd27a5c5785cf'
+auth_token = '67c6417566a35b483a69adb7860b0942'
 client = Client(account_sid, auth_token)
 welcome_message = "HXfdf3119d45c9eddb40ed53c15e3a1716"
 offer_message = "HXbe7bee6c57059a6db3c916ee3bd8ee49"
