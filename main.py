df68e356b4b9fc100c554301ad538bf4d1767a10
AbdelrahmanAhmed04 <abdelrahman.ahmed.mail@gmail.com>
2024-10-24 16:30:59 +0300

    not a big deal

--- main.py
+++ main.py
@@ -87,7 +87,7 @@ def whatsapp_reply():
     message_sid = request.form.get('MessageSid')  # Unique message SID
     profile_name = request.form.get('ProfileName')  # WhatsApp profile name (e.g., 'Bob')
     message_statues = request.form.get('SmsStatus') 
-    log_message_in_dynamodb(from_number, message_body, "incoming", message_sid, profile_name, to_number)
+    log_message_in_dynamodb(from_number, message_body, message_statues + " incoming" , message_sid, profile_name, to_number)
 
     if "run algorithm send messages auto" in message_body:
 
