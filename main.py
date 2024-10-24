a543ebc837fd3fc2e6a240622de7ac68637797e6
AbdelrahmanAhmed04 <abdelrahman.ahmed.mail@gmail.com>
2024-10-24 17:18:28 +0300

    nothing big

--- main.py
+++ main.py
@@ -149,7 +149,7 @@ def whatsapp_reply():
         log_message_in_dynamodb(to_number, error_code, message_status , message_sid, error_message, from_number)
 
     if message_status =="received":
-        log_message_in_dynamodb(from_number, message_body, "outgoing", message.sid, profile_name, to_number)
+        log_message_in_dynamodb(from_number, message_body, "incoming", message.sid, profile_name, to_number)
     
     return "Message sent", 200
 
