110a4901fc0e0e48fca81698b00dbc6605987a0d
AbdelrahmanAhmed04 <abdelrahman.ahmed.mail@gmail.com>
2024-11-01 00:45:17 +0200

    god forgive me

--- main.py
+++ main.py
@@ -99,22 +99,24 @@ def whatsapp_reply():
 ####SETTING UP CUSTOM COMMANDS####
 
     if "run algorithm send messages auto" in message_body:
-        logging.info("Received 'run algorithm send messages auto' command")
+
         pattern = r'\+\d+'
         phone_numbers = re.findall(pattern, message_body)
 
         # Function to send a message to a list of numbers
-        for number in phone_numbers :
-            message = client.messages.create(
-                from_="whatsapp:+18643873878",
-                to=f"whatsapp:{number}",  # Each number should be prefixed with 'whatsapp:'
-                body=phone_numbers,
-            )
-            log_message_in_dynamodb(number, "sent automated welcome message!", "outgoing", message.sid,
+        def send_messages_to_numbers(numbers):
+            for number in numbers:
+                message = client.messages.create(
+                    from_="whatsapp:+18643873878",
+                    to=f"whatsapp:{number}",  # Each number should be prefixed with 'whatsapp:'
+                    content_sid=offer_message,
+                )
+                log_message_in_dynamodb(number, "sent automated welcome message!", "outgoing", message.sid,
                                         profile_name, to_number)
-            logging.info(f"Message sent to {number}")
+                logging.info(f"Message sent to {number}")
 
         # Call the function to send messages
+        send_messages_to_numbers(phone_numbers)
     
     if "send custom message to number" in message_body:
 
